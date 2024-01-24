6a0ba2a1f60b830b845fec6116004c0e2b0bdf51
cburgos89 <cburgos89@gmail.com>
2024-01-23 21:50:13 -0500

    Added css, js, and includes folder with updated files

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Willow Tree Therapy Services, LLC</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <header>
+    <div class="row row-1"></div>
+    <div class="row row-2">
+      <div class="logo"></div>
+      <nav>
+        <ul class="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Resources</a></li>
+          <li><a href="#">Rates/Insurance</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <div id="hero" class="section">
+    <h1>Willow Tree Therapy Services, LLC</h1>
+  </div>
+
+  <div id="welcome" class="section">
+    <h2>Welcome!</h2>
+  </div>
+
+  <div id="short-bio" class="section">
+    <h2>About Amanda Bailey</h2>
+  </div>
+
+  <div id="get-started" class="section">
+    <h2>Get Started!</h2>
+  </div>
+
+  <div id="map" class="section">
+
+  </div>
+
+  <footer>
+    <p>Copyright &copy; Willow Tree Therapy Services, LLC. All rights reserved.</p>
+    <p>Website made by <a href="https://coreyburgos.com" target="_blank" referrerpolicy="no-referer">Corey Burgos</a></p>
+  </footer>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
