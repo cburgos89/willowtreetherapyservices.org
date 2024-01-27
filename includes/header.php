28f938b4fb224da46d2dabbb734a0c5ef0398ac6
cburgos89 <cburgos89@gmail.com>
2024-01-26 23:12:43 -0500

    Added CSS properties

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="row">
+        <div class="logo"></div>
+        <nav>
+            <div class="hamburger"></div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Client Handbook</a></li>
+                <li><a href="#">Resources</a></li>
+                <li><a href="#">Rates/Insurance</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -11,71 +11,67 @@
 <body>
   <div id="container">
 
-    <header>
+    <div class="contact-bar">
       <div class="row">
-        <div class="logo"></div>
-        <nav>
-          <div class="hamburger"></div>
-          <ul class="nav-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Client Handbook</a></li>
-            <li><a href="#">Resources</a></li>
-            <li><a href="#">Rates/Insurance</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
+        <div class="column contact-bar-column-1">
+          <div class="logo">
+            <a href="/">
+              <img src="" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="column contact-bar-column-2">
+          <div class="call-to-action-top">
+            <div class="call-to-action-button-top contact">
+              <a href="#">Contact</a>
+            </div>
+            <div class="call-to-action-button-top schedule">
+              <a href="#">Schedule an Appointment</a>
+            </div>
+          </div>
+          <div class="address-top"></div>
+          <div class="phone-top"></div>
+        </div>
       </div>
-    </header>
+    </div>
+
+    <!-- Header PHP -->
+    <?php include_once('./includes/header.php'); ?>
 
     <main>
-      <div id="hero" class="section">
+      <section id="hero">
         <h1>Willow Tree Therapy Services, LLC</h1>
-      </div>
+      </section>
 
-      <div id="welcome" class="section">
+      <section id="welcome">
         <h2>Welcome!</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe labore sit sequi id, voluptatem adipisci doloribus delectus nihil! Ratione minus corrupti expedita ipsum quia, vel, aliquam voluptatem quasi repellendus nam laudantium ducimus? Omnis error autem doloremque itaque illum maiores! Ad debitis expedita adipisci? Distinctio ex adipisci dignissimos iste iusto accusamus eos alias quos earum necessitatibus. Minima fugiat quae tempora numquam, fuga repudiandae accusamus praesentium ipsum aut quisquam voluptatem rem et explicabo consequatur culpa debitis perspiciatis enim alias quasi? Nisi vitae ex, odit impedit facere debitis voluptate ducimus ab earum natus dolor fuga est nihil a modi, dolore sit inventore architecto quasi aperiam ratione minus enim? Distinctio expedita dolore dolores, architecto deleniti, inventore, rem dolorem repellendus hic mollitia numquam aperiam molestias eos quod culpa maxime. Deserunt aliquam, quae rerum, quod facilis repudiandae magni sequi odio tempore dolorem cupiditate culpa, reprehenderit tempora repellendus quibusdam quia doloremque? Fuga aperiam nesciunt ducimus at voluptate ratione molestiae doloribus quis ex aliquid magnam dolore iure, natus nihil! Autem rem consequatur dolor ut quod vel ea totam, quibusdam laudantium cupiditate consequuntur, eum temporibus numquam. Velit dignissimos obcaecati recusandae veritatis excepturi esse cumque quia tempore! Quasi deserunt ipsa, vero veniam distinctio id aliquid quo ipsam, asperiores commodi consectetur?</p>
-      </div>
+      </section>
 
-      <div id="short-bio" class="section">
+      <section id="short-bio">
         <h2>About Amanda Bailey</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur repudiandae in unde dicta corrupti alias. Exercitationem ullam sunt eos voluptas. Iure harum eveniet rem dolorem? Repellat sequi accusantium eos? Quis corrupti itaque, hic tenetur unde dolor cum nam corporis nostrum quos minus illum culpa veritatis nulla quod soluta sint, porro incidunt aliquid delectus iste fugiat dignissimos. Praesentium rerum aspernatur adipisci velit dolores, sit amet voluptatibus! Nemo dignissimos unde voluptatem pariatur! Praesentium quibusdam blanditiis at illo? Sit labore eum laboriosam ipsam, ullam deleniti iste libero id reprehenderit soluta officia dicta, eius voluptas beatae. Iure dignissimos illo recusandae doloremque consequatur illum dolorum!</p>
-      </div>
+        <div class="row">
+          <div class="column short-bio-column-1">
+            <img src="" alt="">
+          </div>
+          <div class="column short-bio-column-2">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur repudiandae in unde dicta corrupti alias. Exercitationem ullam sunt eos voluptas. Iure harum eveniet rem dolorem? Repellat sequi accusantium eos? Quis corrupti itaque, hic tenetur unde dolor cum nam corporis nostrum quos minus illum culpa veritatis nulla quod soluta sint, porro incidunt aliquid delectus iste fugiat dignissimos. Praesentium rerum aspernatur adipisci velit dolores, sit amet voluptatibus! Nemo dignissimos unde voluptatem pariatur! Praesentium quibusdam blanditiis at illo? Sit labore eum laboriosam ipsam, ullam deleniti iste libero id reprehenderit soluta officia dicta, eius voluptas beatae. Iure dignissimos illo recusandae doloremque consequatur illum dolorum!</p>
+          </div>
+        </div>
+      </section>
 
-      <div id="get-started" class="section">
+      <section id="get-started">
         <h2>Get Started!</h2>
         <div class="row">
           <div class="get-started-card get-started-card-1"></div>
           <div class="get-started-card get-started-card-2"></div>
           <div class="get-started-card get-started-card-3"></div>
         </div>
-      </div>
+      </section>
 
-      <div id="client-info" class="section">
-        <h2>New Client Information</h2>
-        <div class="row">
-          <div class="client-info-card client-info-card-1">
-            <h3>Rates & Insurance</h3>
-            <p>Verify your health insurance coverage here to determine eligibility for our services with your insurance provider. Feel free to contact us if you have any questions.</p>
-            <div class="button">
-              <a href="#">Rates & Insurance</a>
-            </div>
-          </div>
-          <div class="client-info-card client-info-card-2">
-            <h3>Frequently Asked Questions</h3>
-            <p>Explore frequently asked questions about therapy, both specific to our services and general psychotherapy inquiries. Your active engagement and commitment are key to achieving success.</p>
-            <div class="button">
-              <a href="#">FAQs</a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <section id="map">
 
-      <div id="map" class="section">
-
-      </div>
+      </section>
 
       <footer>
         <p>Copyright &copy; Willow Tree Therapy Services, LLC. All rights reserved.</p>
