1fa8746ebf1146874cd4d6006d5b8dca3cffc402
cburgos89 <cburgos89@gmail.com>
2024-02-29 21:44:29 -0500

    Made many changes

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Load initial content for header, aside, and footer
+  loadContent("includes/top-cta.html", "topCtaContent");
+  loadContent("includes/header.html", "headerContent");
+  loadContent("includes/aside.html", "asideContent");
+  loadContent("includes/footer.html", "footerContent");
+
+  // You can add event listeners or additional functionality here
+});
+
+const loadContent = async (page, targetElementId) => {
+  const targetElement = document.getElementById(targetElementId);
+
+  try {
+    // Fetch and load content dynamically
+    const response = await fetch(page);
+    const html = await response.text();
+    targetElement.innerHTML = html;
+  } catch (error) {
+    console.error(`Error loading content for ${targetElementId}:`, error);
+  }
+};
